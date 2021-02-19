1e86809164863a98d78120588317e17ab5e32d12
Solveig <solveig@tml16a.Informatik.Uni-Tuebingen.De>
2021-02-19 17:35:13 +0100

    simple first version with easy toy examples and demos for the three settings

---
+++ src/cut_finding.py
@@ -0,0 +1,347 @@
+import numpy as np
+from sklearn.random_projection import GaussianRandomProjection
+
+
+def a_slice(xs, a):
+    cut_names = []
+    cuts = []
+
+    n, d = np.shape(xs)
+
+    for i in range(d):
+        x = xs[:, i]
+        idx_sorted = np.argsort(np.argsort(x))
+        j = 1
+        while j < n-a-1:
+            cut = np.array([1] * j + [0] * (n-j))
+            cut = cut[idx_sorted].astype(bool)
+            cuts.append(cut)
+            cut_names.append('dimension {} smaller or equal than {}'.format(i, max(x[cut])))
+            j += (a-1)
+
+    return np.array(cuts)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Kernighan-Lin algorithm
+#
+# implemented the pseudocode from https://en.wikipedia.org/wiki/Kernighan–Lin_algorithm
+# also kept all the variable names
+# it's a little difficult to see right away
+# This is now fully vectorized. Everything still seems to work. Somebody should review this, though.
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def initial_partition(xs, fraction=0.5):
+    nb_vertices, _ = xs.shape
+
+    partition = int(round(fraction * nb_vertices))
+
+    A = np.zeros(nb_vertices, dtype=bool)
+    A[np.random.choice(np.arange(nb_vertices), partition, False)] = True
+
+    B = np.logical_not(A)
+
+    return A, B
+
+
+def compute_D_values(xs, A, B):
+    nb_vertices, _ = xs.shape
+
+    D = np.zeros(nb_vertices)
+
+    A_xs = xs[A, :]
+    B_xs = xs[B, :]
+
+    D[A] = np.sum(A_xs[:, B], axis=1) - np.sum(A_xs[:, A], axis=1)
+    D[B] = np.sum(B_xs[:, A], axis=1) - np.sum(B_xs[:, B], axis=1)
+
+    return D
+
+
+def update_D_values(xs, A, B, D):
+    nb_vertices, _ = xs.shape
+
+    A_xs = xs[A, :]
+    B_xs = xs[B, :]
+
+    D[A] = np.sum(A_xs[:, B], axis=1) - np.sum(A_xs[:, A], axis=1)
+    D[B] = np.sum(B_xs[:, A], axis=1) - np.sum(B_xs[:, B], axis=1)
+
+    return D
+
+
+def maximize(xs, A, B, D):
+    Dtiled = np.tile(D, (len(D), 1))
+    g = Dtiled + Dtiled.T - 2 * xs
+
+    mask = np.logical_not(np.outer(A, B))
+    g = np.ma.masked_array(g, mask)
+
+    (a_res, b_res) = np.unravel_index(np.argmax(g), g.shape)
+    g_max = g[a_res, b_res]
+
+    return g_max, a_res, b_res
+
+
+def kernighan_lin(A, nb_cuts, lb_f, seed, verbose, early_stopping):
+    cuts = []
+    np.random.seed(seed)
+
+    for i in range(nb_cuts):
+        f = np.random.uniform(lb_f, 0.5)
+        if verbose >= 3:
+            print(f'\tlooking for cut {i + 1}/{nb_cuts} with f={f:.02}')
+        cut = kernighan_lin_algorithm(A, early_stopping, f)
+        cuts.append(cut)
+
+    cuts = np.array(cuts)
+
+    return cuts
+
+
+def kernighan_lin_algorithm(xs, early_stopping, fraction):
+    nb_vertices, _ = xs.shape
+
+    A, B = initial_partition(xs, fraction)
+
+    i = 0
+    while True:
+        A_copy = A.copy()
+        B_copy = B.copy()
+        xs_copy = xs.copy()
+        D = compute_D_values(xs, A_copy, B_copy)
+        g_max = -np.inf
+        g_acc = 0
+        swap_max = np.empty_like(A)
+        swap_acc = np.zeros_like(A)
+
+        for _ in range(min(sum(A_copy), sum(B_copy))):
+            # greedily find best two vertices to swap
+            g, a, b = maximize(xs_copy, A_copy, B_copy, D)
+
+            swap_acc[a] = True
+            swap_acc[b] = True
+            g_acc += g
+            if g_acc > g_max:
+                g_max = g_acc
+                swap_max[:] = swap_acc[:]
+
+            xs_copy[a, :] = 0
+            xs_copy[:, a] = 0
+            xs_copy[b, :] = 0
+            xs_copy[:, b] = 0
+
+            A_copy[a] = False
+            B_copy[b] = False
+
+            D = update_D_values(xs_copy, A_copy, B_copy, D)
+
+        if g_max > 0:
+            # swapping nodes from initial partition that improve the cut
+            np.logical_not(A, out=A, where=swap_max)
+            np.logical_not(A, out=B)
+
+        else:
+            break
+
+        i += 1
+
+        if early_stopping and i > early_stopping:
+            break
+
+    return A
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Fiduccia-Mattheyses-Algorithm
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+def fid_mat(xs, nb_cuts, lb_f, seed, verbose, early_stopping):
+    cuts = []
+
+    np.random.seed(seed)
+
+    for i in range(nb_cuts):
+        if verbose >= 3:
+            print('\tlooking for cut {}/{}'.format(i+1, nb_cuts))
+        cut = fid_mat_algorithm(xs, lb_f, verbose, early_stopping)
+        cuts.append(cut)
+
+    cuts = np.array(cuts)
+
+    return cuts
+
+
+def fid_mat_algorithm(xs, r, verbose, early_stopping):
+    r = np.random.uniform(r, 0.5)
+    nb_cells, _ = xs.shape
+    A, B = initial_partition(xs, np.random.uniform(r, 0.5))
+
+    cell_array = [np.argwhere(row > 0).flatten() for row in xs]
+
+    # while not converged
+    i = 0
+    while True:
+        A_copy = A.copy()
+        B_copy = B.copy()
+        not_locked = np.full([nb_cells], True)
+        gain_list = compute_initial_gains(A, B, cell_array, xs)
+        g_max = -np.inf
+        g_acc = 0
+        move_max = np.empty_like(A)
+        move_acc = np.zeros_like(A)
+
+        # iterate over all vertices and move them
+        while sum(not_locked > 0):
+            base_cell, g = choose_cell_greedy(A_copy, B_copy, gain_list, r, not_locked)
+
+            if base_cell is None:
+                break
+
+            g_acc += g
+            move_acc[base_cell] = True
+
+            if g_acc > g_max:
+                g_max = g_acc
+                move_max[:] = move_acc[:]
+
+            if A_copy[base_cell]:
+                A_copy, B_copy, gain_list, not_locked = \
+                    move_and_update(base_cell, A_copy, B_copy, gain_list, not_locked, cell_array, xs)
+            else:
+                B_copy, A_copy, gain_list, not_locked = \
+                    move_and_update(base_cell, B_copy, A_copy, gain_list, not_locked, cell_array, xs)
+
+        if g_max > 0:
+            # moving nodes from initial partition that improve the cut
+            np.logical_not(A, out=A, where=move_max)
+            np.logical_not(A, out=B)
+        else:
+            break
+
+        i += 1
+
+        if early_stopping and i > early_stopping:
+            break
+
+    if verbose >= 3:
+        print("\tfinal ratio: {}".format(np.round(sum(A) / nb_cells, 2)))
+
+    return A
+
+
+def compute_initial_gains(A, B, cell_array, xs):
+    nb_cells = len(cell_array)
+    gain_list = np.full(nb_cells, 0)
+
+    for cell_index in range(nb_cells):
+        gain = 0
+        for adj_cell in cell_array[cell_index]:
+            if A[cell_index]:
+                gain += compute_gain_for_net(A, B, adj_cell) * xs[cell_index, adj_cell]
+            elif B[cell_index]:
+                gain += compute_gain_for_net(B, A, adj_cell) * xs[cell_index, adj_cell]
+
+        # add cell to the sorted bucket list
+        gain_list[cell_index] = gain
+
+    return gain_list
+
+
+def compute_gain_for_net(F, T, other_index):
+    Fn = F[other_index] + 1
+    Tn = T[other_index]
+
+    if Fn == 1:
+        return +1
+    elif Tn == 0:
+        return -1
+    else:
+        return 0
+
+
+def choose_cell_greedy(A, B, gain_list, r, not_locked):
+    possible_partition = is_balanced(A, r)
+
+    idx = np.arange(len(gain_list))[not_locked]
+    cells = gain_list[not_locked]
+
+    # choose cell that is not locked, does not harm the ratio and maximizes the gain
+    for cell in idx[np.argsort(-cells)]:
+        partition = [A[cell], B[cell]]
+        if not_locked[cell] & np.logical_and(partition, possible_partition).any():
+            return cell, gain_list[cell]
+
+    return None, 0
+
+
+def is_balanced(A, r):
+    sumA = sum(A)
+    cardinalityA_1 = sumA - 1
+    cardinalityA_2 = sumA + 1
+
+    W = len(A)
+
+    leftbound = r * W
+    rightbound = W - r * W
+    return [leftbound <= cardinalityA_1 <= rightbound, leftbound <= cardinalityA_2 <= rightbound]
+
+
+def move_and_update(base_cell, F, T, gain_list, not_locked, cell_array, xs):
+    # lock base cell
+    not_locked[base_cell] = False
+    # switch block
+    F[base_cell] = 0
+    T[base_cell] = 1
+
+    # increment or decrement gain of neighbouring cells
+    for other_cell in cell_array[base_cell]:
+        # check critical nets before move
+        Tn = T[other_cell]
+        Fn = F[other_cell] + 1
+        if not_locked[other_cell]:
+            if Tn == 0:
+                gain_list = adjust_gain(gain_list, other_cell, +1 * xs[base_cell, other_cell])
+            elif Tn == 1:
+                gain_list = adjust_gain(gain_list, other_cell, -1 * xs[base_cell, other_cell])
+
+            # # chance net distribution to reflect the move
+            Tn += 1
+            Fn -= 1
+            if Fn == 0:
+                gain_list = adjust_gain(gain_list, other_cell, -1 * xs[base_cell, other_cell])
+            elif Fn == 1:
+                gain_list = adjust_gain(gain_list, other_cell, +1 * xs[base_cell, other_cell])
+
+    return F, T, gain_list, not_locked
+
+
+def adjust_gain(g_list, cell, value):
+    g_list[cell] += value
+    return g_list
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# random projection and 2 means
+# ----------------------------------------------------------------------------------------------------------------------
+
+def random_projection_mean(xs, nb_cuts, seed):
+    cuts = np.empty([nb_cuts, xs.shape[0]], dtype=bool)
+
+    np.random.seed(seed)
+
+    for c in range(nb_cuts):
+
+        seed = np.random.randint(100)
+
+        projection = GaussianRandomProjection(n_components=1, random_state=seed).fit_transform(xs).reshape(-1)
+
+        cut_value = np.mean(projection)
+
+        cut = projection < cut_value
+
+        cuts[c] = cut.astype(bool)
+
+    return cuts
---
+++ src/utils.py
@@ -0,0 +1,196 @@
+import hashlib
+import json
+
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.neighbors._dist_metrics import DistanceMetric
+
+
+class Orientation(object):
+
+    def __init__(self, direction):
+        self.orientation_bool = direction
+        if direction == 'both':
+            self.direction = direction
+        elif direction is True:
+            self.direction = 'left'
+        elif direction is False:
+            self.direction = 'right'
+
+    def __eq__(self, value):
+
+        if self.direction == value.direction:
+            return True
+
+        return False
+
+    def __str__(self):
+        if self.direction == 'both':
+            return self.direction
+        elif self.direction == 'left':
+            return 'True'
+        elif self.direction == 'right':
+            return 'False'
+
+
+def normalize(array):
+    """
+    Normalize a 1d numpy array between [0,1]
+
+    Parameters
+    ----------
+    array: ndarray
+        the array to normalize
+
+    Returns
+    -------
+    ndarray
+        the normalized array
+    """
+
+    ptp = np.ptp(array)
+    if ptp != 0:
+        return (array - np.min(array)) / np.ptp(array)
+    else:
+        return np.ones_like(array)
+
+
+def matching_items(d1, d2):
+    matching_keys = []
+    common_keys = d1.keys() & d2.keys()
+    for k in common_keys:
+        if d1[k] == d2[k]:
+            matching_keys.append(k)
+
+    return matching_keys
+
+
+def merge_dictionaries_with_disagreements(d1, d2):
+    merge = {**d1, **d2}
+    common_keys = d1.keys() & d2.keys()
+
+    for k in common_keys:
+        if d1[k] != d2[k]:
+            merge.pop(k)
+
+    return merge
+
+
+def get_points_to_plot(xs, cs):
+    """
+    Calculate embedding of points for visualization
+
+    Parameters
+    ----------
+    xs: ndarray
+        the datapoints
+    cs: ndarray
+        the centers of the clusters if method supports
+
+    Returns
+    -------
+    xs_embedded:
+         embedding of points in two dimensions
+    cs_embedded:
+         embedding of centers if method supports
+    """
+    _, nb_features = xs.shape
+
+    nb_centers = None
+    cs_embedded = None
+    if cs is not None:
+        nb_centers, _ = cs.shape
+
+    if nb_features > 2:
+        if cs is not None:
+            points_to_embed = np.vstack([xs, cs])
+            embeds = TSNE(n_components=2, random_state=42).fit_transform(points_to_embed)
+            xs_embedded, cs_embedded = embeds[:-nb_centers], embeds[-nb_centers:]
+        else:
+            xs_embedded = TSNE(n_components=2, random_state=42).fit_transform(xs)
+    else:
+        xs_embedded = xs
+        cs_embedded = cs
+
+    if cs is not None:
+        return xs_embedded, cs_embedded
+    else:
+        return xs_embedded, None
+
+
+def subset(a, b):
+    return (a & b).count() == a.count()
+
+
+def compute_cost_and_order_cuts(bipartitions, cost_function):
+    """
+    Compute the cost of a series of cuts and costs them according to their cost
+
+    Parameters
+    ----------
+    cuts: Cuts
+        the cuts that we will consider
+    cost_function: function
+        The order function
+
+    Returns
+    -------
+    cuts: Cuts
+        the cuts ordered by costs
+    """
+
+    cost_bipartitions = np.zeros(len(bipartitions.values), dtype=float)
+    for i_cut, cut in enumerate(bipartitions.values):
+        cost_bipartitions[i_cut] = cost_function(cut)
+
+    idx = np.argsort(cost_bipartitions)
+
+    bipartitions.values = bipartitions.values[idx]
+    bipartitions.costs = cost_bipartitions[idx]
+    if bipartitions.names is not None:
+        bipartitions.names = bipartitions.names[idx]
+    if bipartitions.equations is not None:
+        bipartitions.equations = bipartitions.equations[idx]
+
+    bipartitions.order = np.argsort(idx)
+
+    return bipartitions
+
+
+def compute_hard_predictions(condensed_tree, cuts, xs=None):
+    if xs is not None:
+        cs = []
+        nb_cuts = len(cuts.values)
+
+        for leaf in condensed_tree.maximals:
+            c = np.full(nb_cuts, 0.5)
+            tangle = leaf.tangle
+            c[list(tangle.specification.keys())] = np.array(list(tangle.specification.values()), dtype=int)
+            cs.append(c[cuts.order])
+
+        cs = np.array(cs)
+
+        return compute_mindset_prediciton(xs, cs), cs
+
+    else:
+        if not condensed_tree.processed_soft_prediction:
+            print("No probabilities given yet. Calculating soft predictions first!")
+
+        probabilities = []
+        for node in condensed_tree.maximals:
+            probabilities.append(node.p)
+
+        ys_predicted = np.argmax(probabilities, axis=0)
+
+        return ys_predicted, None
+
+def compute_mindset_prediciton(xs, cs):
+    metric = DistanceMetric.get_metric('manhattan')
+
+    distance = metric.pairwise(xs, cs)
+    predicted = np.empty(xs.shape[0])
+
+    for i, d in enumerate(distance):
+        predicted[i] = np.random.choice(np.flatnonzero(d == d.min()))
+
+    return predicted
